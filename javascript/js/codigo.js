3d0bc15cbec02fde872283b2e88bcd63db25444f
josesosa24 <jasm.somart@hotmail.com>
2016-04-12 10:59:17 -0600

    Archivo Js, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,9 @@
+var botonClick=function(){
+	alert("Click del boton");
+}
+
+var nombre="JOSE";
+document.write("<strong> JS en accion </strong>"+" "+nombre);
+console.log("a punto de lanzar un mensaje")
+alert("Mensaje por pantalla");
+
