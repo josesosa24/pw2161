2dbcf7af32c1fb1714780ac76748ced9cbc538c1
josesosa24 <jasm.somart@hotmail.com>
2016-04-14 22:31:46 -0600

    Calculadora con estilos

--- calculadora/javascript/calculadora.js
+++ calculadora/javascript/calculadora.js
@@ -1,6 +1,13 @@
 
 var operador="";
 
+function borrar() {
+	document.calculadora.operando1.value='0';
+	document.calculadora.operando2.value='0';
+	document.calculadora.resultado.value='0';
+//	document.calculadora.operando1.focus();
+}
+
 function igual() {
 	var valor1=document.calculadora.operando1.value;
 	var valor2=document.calculadora.operando2.value;
