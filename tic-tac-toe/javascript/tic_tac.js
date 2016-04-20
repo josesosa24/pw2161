cf8d7627ea6923f8f1b92e7f164b94e9f711cfc3
josesosa24 <jasm.somart@hotmail.com>
2016-04-19 23:28:34 -0600

    Tic tac toe almost working

---
+++ tic-tac-toe/javascript/tic_tac.js
@@ -0,0 +1,154 @@
+var countclicks=0;
+var clicked;
+
+function putsomething1() {
+	var check1= document.getElementById('td1').value;
+	
+	if(check1==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td1').innerHTML='X';
+		}
+		else {
+			document.getElementById('td1').innerHTML='O';
+		}
+
+		countclicks++;
+	}
+	else {
+		
+	}
+
+}
+
+function putsomething2() {
+
+	var check2= document.getElementById('td2').value;
+	
+	if(check2==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td2').innerHTML='X';
+		}
+		else {
+			document.getElementById('td2').innerHTML='O';
+		}
+	}
+	countclicks++;
+}
+
+
+function putsomething3() {
+
+	var check3= document.getElementById('td3').value;
+
+	if(check3==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td3').innerHTML='X';
+		}
+		else {
+			document.getElementById('td3').innerHTML='O';
+		}
+	}
+		countclicks++;
+}
+
+
+function putsomething4() {
+	var check4= document.getElementById('td4').value;
+
+	if(check4==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td4').innerHTML='X';
+		}
+		else {
+			document.getElementById('td4').innerHTML='O';
+		}
+	}
+	countclicks++;
+
+}
+
+
+function putsomething5() {
+		
+	var check5= document.getElementById('td5').value;
+
+	if(check5==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td5').innerHTML='X';
+		}
+		else {
+			document.getElementById('td5').innerHTML='O';
+		}
+	}
+	countclicks++;
+}
+
+
+function putsomething6() {
+	var check6= document.getElementById('td6').value;
+
+	if(check6==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td6').innerHTML='X';
+		}
+		else {
+			document.getElementById('td6').innerHTML='O';
+		}
+	}
+	countclicks++;
+
+}
+
+
+function putsomething7() {
+	
+	var check7= document.getElementById('td7').value;
+
+	if(check7==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td7').innerHTML='X';
+		}
+		else {
+			document.getElementById('td7').innerHTML='O';
+		}
+	}
+	countclicks++;
+}
+
+function putsomething8() {
+	
+	var check8= document.getElementById('td8').value;
+	
+	if(check8==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td8').innerHTML='X';
+		}
+		else {
+			document.getElementById('td8').innerHTML='O';
+		}
+	}
+	countclicks++;
+}
+
+function putsomething9() {
+
+	var check9= document.getElementById('td9').value;
+
+	if(check9==null) {
+		if(countclicks%2 == 0) {
+			document.getElementById('td9').innerHTML='X';
+		}
+		else {
+			document.getElementById('td9').innerHTML='O';
+		}
+	}
+	countclicks++;
+}
+
+	
+
+
+		
+		
+		
+
