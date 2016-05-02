90f905c7ff254b7b2c8061757e74c7ce2ca86f0d
josesosa24 <jasm.somart@hotmail.com>
2016-05-02 10:58:22 -0600

    Keypress event

--- jquery/js/eventos.js
+++ jquery/js/eventos.js
@@ -6,11 +6,24 @@ var inicio= function() {
 	//Preparar los eventos de todos los objetos
 	var clicBoton= function() {
 		console.log("Clic del botón")
-		// $(".anuncioWeb").html("Clic del boton")
+		$(".anuncioWeb").html("Clic del boton")
 		$(".anuncioWeb").append("Clic del boton") //Agrega al contenido que ya existe
+
+	}
+	var clicBoton2 =function() {
+		alert("Boton 2")
+	}
+	var teclaUnInput= function(tecla) {
+		if(tecla.which==13 ) {
+			//que se posicione en otroInput 
+			$("#otroInput").focus();
+		}
 	}
 
-	$("#miBoton").on("click", clicBoton)
+	$("#miBoton").off("click", clicBoton)
+	$("#miBoton").on("click", clicBoton2)
+	$("#unInput").on("keypress", teclaUnInput)
+
 }
 
 $(document).on("ready", inicio)
